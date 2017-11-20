ddbf394631e335d3c1842ff4c77b618e8c486344
tenthirtyone <Spactigus@gmail.com>
2017-11-20 01:02:50 -0500

    Bitcoin Client started, util.parsemn, filestorage, and cli classes. Bin w/ symlinks to mirrow old ux.

--- config.js
+++ config.js
@@ -1,18 +1,24 @@
 'use strict';
 
-const mkdirp = require('mkdirp');
+const mkdirp                        = require('mkdirp');
 const { writeFileSync, existsSync } = require('fs');
-const { homedir } = require('os');
-const { join } = require('path');
+const { homedir }                   = require('os');
+const { join }                      = require('path');
+
 const datadir = join(homedir(), '.config/walletclient');
 
-
 module.exports = {
-
+  Wallet: {
+    host: '127.0.0.1',
+  },
+  FileStorage: {
+    homeDir: homedir(),
+    fileName: 'default-wallet.dat',
+  }
 
 };
 
 if (!existsSync(join(datadir, 'config'))) {
   mkdirp.sync(datadir);
-  writeFileSync(join(datadir, 'config'), ini.stringify(module.exports));
+  writeFileSync(join(datadir, 'config'), JSON.stringify(module.exports));
 }
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const cli = require('./lib/cli');
+const config = require('./config');
+
+const wallet = new cli(config);
+
